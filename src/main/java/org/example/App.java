ea429faa1d53041984eb266d559738504ab9ffa2
Dany Meyer <dany.meyer@hnu.de>
2025-11-05 16:56:59 +0100

    Add greetStudent method

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,10 +4,14 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello from IntelliJ + Git!");
         sayHello("Team");
+        greetStudent("Alice", " git");
     }
 
     public static void sayHello(String name) {
         System.out.println("Hello, " + name);
+    }
+    public static void greetStudent(String name, String course) {
+        System.out.println("Welcome " + name + " to the course " + course + "!");
     }
 }
 
