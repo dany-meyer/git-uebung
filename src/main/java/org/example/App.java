226bb600a8b29085ec631b46f7bed9e82bec0169
Dany Meyer <dany.meyer@hnu.de>
2025-11-05 16:29:10 +0100

    initial

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from IntelliJ + Git!");
+        sayHello("Team");
+    }
+
+    public static void sayHello(String name) {
+        System.out.println("Hello, " + name + " 👋");
+    }
+}
+
