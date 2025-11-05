cf736803448b3ffd8e0ac89b55b23d47983cae47
Ynad <122452582+dany-meyer@users.noreply.github.com>
2025-11-05 16:44:46 +0100

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,7 +7,7 @@ public class App {
     }
 
     public static void sayHello(String name) {
-        System.out.println("Hello, " + name + " 👋");
+        System.out.println("Hello, " + name);
     }
 }
 
