060c5dea71a3b14155150f692eae305bc7a42c79
Dany Meyer <dany.meyer@hnu.de>
2025-11-06 11:59:08 +0100

    add method greetStudent_dany

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,11 +4,16 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Studis from IntelliJ + Git!");
         sayHello("Team");
+        greetStudent_dany("Susi", "Software Engineering");
     }
 
     public static void sayHello(String name) {
         System.out.println("Hello, " + name);
     }
 
+
+    public static void greetStudent_dany(String name, String course) {
+        System.out.println("Welcome " + name + " to the course " + course + "!");
+    }
 }
 
