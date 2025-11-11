f3aee4518c221b3976bb6d066a0f59a01ddeeba4
Dany Meyer <dany.meyer@hnu.de>
2025-11-11 15:46:48 +0100

    Add greetStudent method

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -19,7 +19,7 @@ public class App {
     }
 
     public void newMethod() {
-        System.out.println("New Method, hier ändere ich den Text");
+        System.out.println("New Method und das wird auch eine Textänderung");
     }
 }
 
